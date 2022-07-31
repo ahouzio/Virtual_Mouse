d6a37e93ca0f4ccc57a2962128316715c0e0f810
Oussama Ahouzi <70753395+Ousama-gif@users.noreply.github.com>
2022-07-31 15:05:45 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# creates a virtual mouse that you can manipulate with your hands
+import cv2
+import mediapipe as mp
+import pyautogui
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_w,screen_h = pyautogui.size()
+index_y = 0
+while True :
+    _,frame = cap.read()
+    frame = cv2.flip(frame,1)
+    frame_height,frame_width,_ = frame.shape
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    
+    if hands :
+        for hand in hands :
+            drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+            for id, landmark in enumerate(landmarks):
+                x = int(landmark.x*frame_width)
+                y = int(landmark.y*frame_height)
+                print(x,y)
+                if id == 8 :
+                    cv2.circle(img = frame, center = (x,y),radius = 10,color = (0,255,255))
+                    index_x = screen_w/frame_width * x
+                    index_y = screen_h/frame_height * y
+                    pyautogui.moveTo(x,y)
+                if id == 4:
+                    cv2.circle(img = frame, center = (x,y),radius = 10,color = (0,255,255))
+                    thumb_x = screen_w/frame_width * x
+                    thumb_y = screen_h/frame_height * y
+                    if abs(index_y - thumb_y) < 20:
+                        pyautogui.click()
+                        pyautogui.sleep(1)
+                    
+    print(hands)
+    cv2.imshow('Virtual Mouse', frame)
+    
+    cv2.waitKey(1)
